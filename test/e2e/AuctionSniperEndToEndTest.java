7793c417552b0a968cba45dca0fda0c2f0c4ab1d
kanatatsu64 <kanatatsu64@gmail.com>
2024-05-08 00:37:54 +0900

    環境構築

---
+++ test/e2e/AuctionSniperEndToEndTest.java
@@ -0,0 +1,30 @@
+package test.e2e;
+
+import org.junit.After;
+import org.junit.Test;
+import auctionsniper.FakeAuctionServer;
+import auctionsniper.ApplicationRunner;
+
+public class AuctionSniperEndToEndTest {
+  private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+  private final ApplicationRunner application = new ApplicationRunner();
+
+  @Test
+  public void sniperJoinsAuctionUnitlAuctionCloses() throws Exception {
+    auction.startSellingItem();
+    application.startBiddingIn(auction);
+    auction.hasReceivedJoinRequestFromSniper();
+    auction.annouceClosed();
+    application.showsSniperHasLostAuction();
+  }
+
+  @After
+  public void stopAuction() {
+    auction.stop();
+  }
+
+  @After
+  public void stopApplication() {
+    application.stop();
+  }
+}
